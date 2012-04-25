a2032b15a23f2fb3f0b1086bfbbf89d237695594
Matthew Churilla <ltguide@gmail.com>
2012-04-25 03:18:47 -0400

    change SourceFilenameFilter to understand * for folders

--- src/ltguide/base/data/SourceFilenameFilter.java
+++ src/ltguide/base/data/SourceFilenameFilter.java
@@ -7,24 +7,30 @@ import java.util.List;
 public class SourceFilenameFilter implements FilenameFilter {
 	private final List<String> folders;
 	private final List<String> types;
+	private boolean allFolders = false;
 	
 	public SourceFilenameFilter(final List<String> folders, final List<String> types) throws IllegalArgumentException {
 		if (folders == null && types == null) throw new IllegalArgumentException();
 		
 		this.folders = folders;
 		this.types = types;
+		
+		if (folders != null && folders.contains("*")) allFolders = true;
 	}
 	
 	@Override
-	public boolean accept(final File dir, String name) {
+	public boolean accept(final File dir, final String name) {
 		final String path = dir.getName().toLowerCase();
-		name = name.toLowerCase();
+		final String nameLower = name.toLowerCase();
 		
-		for (final String folder : folders)
+		if (allFolders) {
+			if (!".".equals(path) || new File(dir, name).isDirectory()) return false;
+		}
+		else for (final String folder : folders)
 			if (path.indexOf(folder) == 0) return false;
 		
 		for (final String type : types)
-			if (name.endsWith(type)) return false;
+			if (nameLower.endsWith(type)) return false;
 		
 		return true;
 	}
